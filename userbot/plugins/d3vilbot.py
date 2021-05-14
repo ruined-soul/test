ce5a7fbca3f3b5cdbc6ddd01f377be64de0b33ee
D3_krish <83129000+D3KRISH@users.noreply.github.com>
2021-05-14 08:46:35 +0530

    Update d3vilbot.py

--- userbot/plugins/d3vilbot.py
+++ userbot/plugins/d3vilbot.py
@@ -12,7 +12,7 @@ from mafiabot.utils import admin_cmd
 import asyncio
 from telethon import events
 
-@borg.on(admin_cmd(pattern="hellbot ?(.*)"))
+@borg.on(admin_cmd(pattern="d3vilbot ?(.*)"))
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.edit("Click [here](https://github.com/D3KRISH/D3VIL-BOT) to open this 🔥**Lit AF!!**🔥 **ɖ3ʋɨʟɮօȶ** Repo.. Join channel :- @D3VIL_BOT_SUPPORT Repo Uploaded By @EAGLE_MAFIA_USERBOT")
