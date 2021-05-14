4e5b35d64f18a972b64f28c031853d13fd0b52d3
D3_krish <83129000+D3KRISH@users.noreply.github.com>
2021-05-14 08:45:06 +0530

    Update and rename hellbot.py to d3vilbot.py

---
+++ userbot/plugins/d3vilbot.py
@@ -0,0 +1,21 @@
+# this plugin made by Eagle Mafia Userbot
+
+"""Plugin for D3vilBot Repo
+
+\nCode by @EAGLE_MAFIA_USERBOT
+
+type '.d3vilbot' to get D3vilBot repo
+"""
+
+import random, re
+from mafiabot.utils import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="hellbot ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("Click [here](https://github.com/D3KRISH/D3VIL-BOT) to open this 🔥**Lit AF!!**🔥 **ɖ3ʋɨʟɮօȶ** Repo.. Join channel :- @D3VIL_BOT_SUPPORT Repo Uploaded By @EAGLE_MAFIA_USERBOT")
+    
+        
+   
