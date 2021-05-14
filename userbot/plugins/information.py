9ace57e04c92211a29ccf72b161e8b5056cf5774
D3_krish <83129000+D3KRISH@users.noreply.github.com>
2021-05-14 09:00:43 +0530

    Update information.py

--- userbot/plugins/information.py
+++ userbot/plugins/information.py
@@ -61,7 +61,7 @@ async def _(event):
 <b>🤖 BOT</b>: {}
 <b>👥 Groups in Common</b>: {}
 
-<b>⚡ <a href='https://t.me/MafiaBot_Support'>From DataBase of MafiaBot</a> ⚡ </b>
+<b>⚡ <a href='https://t.me/EAGLE_MAFIA_USERBOT'>From DataBase of MafiaBot</a> ⚡ </b>
 """.format(
         user_id,
         user_id,
