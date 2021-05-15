78af5f199e7f21b264bef478cf1f111e1089697b
D3_krish <83129000+D3KRISH@users.noreply.github.com>
2021-05-15 11:22:12 +0530

    Update heroku.py

--- userbot/plugins/heroku.py
+++ userbot/plugins/heroku.py
@@ -187,7 +187,7 @@ async def _(dyno):
         thumb = mafia_logo
     except:
         return await dyno.reply(
-            " Please make sure your Heroku API Key, Your App name are configured correctly in the heroku\n\n[Visit Support Group For Help](https://t.me/MafiaBot_Chit_Chat)"
+            " Please make sure your Heroku API Key, Your App name are configured correctly in the heroku\n\n[Visit Support Group For Help](https://t.me/EAGLE_MAFIA_USERBOT)"
         )
     mafia_data = app.get_log()
     mafia_key = (
