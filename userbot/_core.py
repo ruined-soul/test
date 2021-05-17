e0803b9fcb4198455fc81075c0adfeaa8df9f35d
D3_krish <83129000+D3KRISH@users.noreply.github.com>
2021-05-17 17:49:06 +0530

    Update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -11,7 +11,7 @@ from userbot import bot as mafiabot
 
 DELETE_TIMEOUT = 5
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Mafia User"
-mafia_logo = "H1M4N5HU0P/IMG_20210517_133913_921.jpg
+mafia_logo = "H1M4N5HU0P/IMG_20210517_133913_921.jpg"
 "
 h1m4n5hu0p = mafiabot.uid
 mafia = f"[{DEFAULTUSER}](tg://user?id={h1m4n5hu0p})"
